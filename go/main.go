ee97504767a7254c675fbb693d46cabe589b7b3d
Abhi <abhishek.chaudhry@unity3d.com>
2020-10-27 16:59:27 -0400

    day1 success

--- go/main.go
+++ go/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+
+	file,err := os.Open("input/day1.txt")
+	if err != nil{
+		panic(err)
+	}
+	defer file.Close()
+
+	fuel := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		line := scanner.Text()
+		var mass int
+		_, err := fmt.Sscanf(line,"%d", &mass)
+		if err != nil {
+			panic(err)
+		}
+
+		fuel += (mass/3) -2
+	}
+
+	fmt.Println(fuel)
 }
